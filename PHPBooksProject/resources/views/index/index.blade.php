fd216104ff7d89fa5e2c3c62f7192c1173b0ed9d
MartinTT00 <61382773+MartinTT00@users.noreply.github.com>
2021-01-20 18:52:05 +0200

    Modify template and add listing

--- PHPBooksProject/resources/views/index/index.blade.php
+++ PHPBooksProject/resources/views/index/index.blade.php
@@ -1,19 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="main">
-			<div class="container">
-				<div class="row">
-					<div class="12u">
-						<section> 
-							<header>
-                                <h2>{{$title}}</h2>
-                                <h2>erererere</h2>
-                                {{$text}}
-							</header>
-						</section>  
-					</div>
-				</div>
-			</div>
-		</div>
+    <div class="inner">
+        <div class="content">
+            <header>
+				<h2>{{$title}}</h2>
+				
+                <hr>
+				@foreach($books as $book)
+                <p>Name of the Book: {{$book->name}}</p>
+				<p>Year of Publishment: {{$book->yearOfPublishment}}</p>
+				<p>Genre of the Book:	{{$book->genre->name}} </p>
+				<p>Author of the Book:	{{$book->author->name}}   <img src="{{$book->author->image}}" width="30px" height="30px"> </p>
+
+                <hr>
+			@endforeach
+			
+            </header>
+            {!! $text !!}
+        </div>
+    </div>
 @endsection
--- PHPBooksProject/resources/views/layouts/app.blade.php
+++ PHPBooksProject/resources/views/layouts/app.blade.php
@@ -1,165 +1,86 @@
 <!DOCTYPE HTML>
 <!--
-	Autonomy by TEMPLATED
-    templated.co @templatedco
-    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+	Industrious by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 -->
 <html>
 	<head>
-		<title>Autonomy by TEMPLATED</title>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Generic Page - Industrious by TEMPLATED</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
-		<link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
-		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-panels.min.js"></script>
-		<script src="js/init.js"></script>
-		<noscript>
-			<link rel="stylesheet" href="css/skel-noscript.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-desktop.css" />
-		</noscript>
-		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
-		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
 	</head>
-	<body class="homepage">
+	<body class="is-preload">
 
 		<!-- Header -->
-		<div id="header">
-			<div class="container">
+			<header id="header">
+				<a class="logo" href="index.html">Industrious</a>
+				<nav>
+					<a href="#menu">Menu</a>
+				</nav>
+			</header>
 
-				<!-- Logo -->
-				<div id="logo">
-					<h1><a href="#">Autonomy</a></h1>
-				</div>
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="elements.html">Elements</a></li>
+					<li><a href="generic.html">Generic</a></li>
+				</ul>
+			</nav>
 
-				<!-- Nav -->
-			<!--	<nav id="nav">
-					<ul>
-						<li class="active"><a href="index.html">Homepage</a></li>
-						<li><a href="twocolumn1.html">Left Sidebar</a></li>
-						<li><a href="twocolumn2.html">Right Sidebar</a></li>
-						<li><a href="onecolumn.html">No Sidebar</a></li> 
-					</ul>
-				</nav> -->
-
+		<!-- Heading -->
+			<div id="heading" >
+				<h1>Generic Page</h1>
 			</div>
-		</div>
 
-		<div id="banner">&nbsp;</div>
+		<!-- Main -->
+			<section id="main" class="wrapper">
+			@yield('content')
+			</section>
 
-		<div id="featured">
-			<div class="container">
-				<div class="row">
-
-					<div class="3u">
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<div class="content">
 						<section>
-							<a href="#" class="image full"><img src="images/pics02.jpg" alt=""></a>
-							<header>
-								<h2>111111111111111111111</h2>
-							</header>
-							<p>Pellentesque viverra vulputate enim. Aliquam erat volutpat. Pellentesque tristique ante. Sed vel tellus.</p>				
+							<h3>Accumsan montes viverra</h3>
+							<p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing. Lorem ipsum dolor vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing sed feugiat eu faucibus. Integer ac sed amet praesent. Nunc lacinia ante nunc ac gravida.</p>
 						</section>
-					</div>
-
-					<div class="3u">
 						<section>
-							<a href="#" class="image full"><img src="images/pics03.jpg" alt=""></a>
-							<header>
-								<h2>222222222222</h2>
-							</header>
-							<p>Pellentesque tristique ante. Sed vel tellus. Curabitur sem urna, consequat suscipit mattis placerat, nulla.</p>				
-						</section>
-					</div>
-
-					<div class="3u">
-						<section>
-							<a href="#" class="image full"><img src="images/pics04.jpg" alt=""></a>
-							<header>
-								<h2>33333333333</h2>
-							</header>
-							<p>Pellentesque viverra vulputate enim. Aliquam erat volutpat. Curabitur sem urna, consequat mattis placerat, nulla.</p>				
-						</section>
-					</div>
-
-					<div class="3u">
-						<section>
-							<a href="#" class="image full"><img src="images/pics05.jpg" alt=""></a>
-							<header>
-								<h2>44444444</h2>
-							</header>
-							<p>Pellentesque viverra vulputate enim. Aliquam erat volutpat. Curabitur sem urna, consequat mattis placerat, nulla.</p>				
-						</section>
-					</div>
-
-				</div>
-                <li class="active"><a href="#">Top</a></li>
-			</div>
-        </div>
-        @yield('content')   
-<!--		<div id="marketing">
-			<div class="container">
-				<div class="row">
-					<div class="3u">
-						<section>
-
-							<header>
-								<h2>5555555</h2>
-							</header>
-							<ul class="style1">
-								<li class="first"><img src="images/pics06.jpg" width="80" height="80" alt="">
-									<p>Nullam non wisi a sem eleifend. Donec mattis libero eget urna. </p>
-								</li>
-								<li><img src="images/pics07.jpg" width="80" height="80" alt="">
-									<p>Nullam non wisi a sem eleifend. Donec mattis libero eget urna. </p>
-								</li>
-								<li><img src="images/pics08.jpg" width="80" height="80" alt="">
-									<p>Nullam non wisi a sem eleifend. Donec mattis libero eget urna. </p>
-								</li>
-							</ul>
-						</section> 
-					</div>
-					<div class="3u">
-						<section>
-							<header>
-								<h2>Maecenas lectus</h2>
-							</header>
-							<ul class="style1">
-								<li class="first"><img src="images/pics09.jpg" width="80" height="80" alt="">
-									<p>Nullam non wisi a sem eleifend. Donec mattis libero eget urna. </p>
-								</li>
-								<li><img src="images/pics10.jpg" width="80" height="80" alt="">
-									<p>Nullam non wisi a sem eleifend. Donec mattis libero eget urna. </p>
-								</li>
-								<li><img src="images/pics11.jpg" width="80" height="80" alt="">
-									<p>Nullam non wisi a sem eleifend. Donec mattis libero eget urna. </p>
-								</li>
+							<h4>Sem turpis amet semper</h4>
+							<ul class="alt">
+								<li><a href="#">Dolor pulvinar sed etiam.</a></li>
+								<li><a href="#">Etiam vel lorem sed amet.</a></li>
+								<li><a href="#">Felis enim feugiat viverra.</a></li>
+								<li><a href="#">Dolor pulvinar magna etiam.</a></li>
 							</ul>
 						</section>
-					</div> 
-				<div class="6u">
 						<section>
-							<header>
-								<h2>Aenean elementum facilisis ligula</h2>
-							</header>
-							<a href="#" class="image full"><img src="images/pics12.jpg" alt=""></a>
-							<p>Consectetuer adipiscing elit. Nam pede erat, porta eu, lobortis eget, tempus et, tellus. Etiam neque. Vivamus consequat lorem at nisl. Nullam non wisi a sem semper eleifend. Donec mattis libero eget urna. Duis pretium velit ac mauris. Proin eu wisi suscipit nulla suscipit interdum. Aenean lectus lorem, imperdiet. Donec mattis libero eget urna. Duis pretium velit ac mauris.</p>
+							<h4>Magna sed ipsum</h4>
+							<ul class="plain">
+								<li><a href="#"><i class="icon fa-twitter">&nbsp;</i>Twitter</a></li>
+								<li><a href="#"><i class="icon fa-facebook">&nbsp;</i>Facebook</a></li>
+								<li><a href="#"><i class="icon fa-instagram">&nbsp;</i>Instagram</a></li>
+								<li><a href="#"><i class="icon fa-github">&nbsp;</i>Github</a></li>
+							</ul>
 						</section>
-					</div> 
+					</div>
+					<div class="copyright">
+						&copy; Untitled. Photos <a href="https://unsplash.co">Unsplash</a>, Video <a href="https://coverr.co">Coverr</a>.
+					</div>
 				</div>
-			</div>
-		</div> -->
+			</footer>
 
-
-
-		<!-- Copyright -->
-		<div id="copyright">
-			<div class="container">
-				Design: <a href="http://templated.co">TEMPLATED</a> Images: <a href="http://unsplash.com">Unsplash</a> (<a href="http://unsplash.com/cc0">CC0</a>)
-			</div>
-		</div>
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
 
 	</body>
 </html>
